7d0bb6af1014458c68d9217880d4f4a6a5f48ea4
Gaurav Tak <gaurav.tak@roro.io>
2023-08-18 23:35:02 +0530

    ridZero corrections implemented

--- app/src/main/java/io/gauravtak/arithmetic_calculator_with_compose/DoubleExtensions.kt
+++ app/src/main/java/io/gauravtak/arithmetic_calculator_with_compose/DoubleExtensions.kt
@@ -1,5 +1,14 @@
 package io.gauravtak.arithmetic_calculator_with_compose
 
+import kotlin.math.ceil
+import kotlin.math.floor
+
 fun Double.ridZero() : String {
+    val intValue = this.toInt()
+    val floorValue = floor(this).toInt()
+    val ceilValue = ceil(this).toInt()
+    if (floorValue == ceilValue && intValue == floorValue) {
+        return intValue.toString()
+    }
     return this.toString()
 }
