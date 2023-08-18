3f88485aa8d9f8b0570d6fb49fb32b784c97782f
Gaurav Tak <gaurav.tak@roro.io>
2023-08-19 00:19:30 +0530

    minor ui corrected

--- app/src/main/java/io/gauravtak/arithmetic_calculator_with_compose/composable_uis/CalculatorUI.kt
+++ app/src/main/java/io/gauravtak/arithmetic_calculator_with_compose/composable_uis/CalculatorUI.kt
@@ -1,13 +1,13 @@
 package io.gauravtak.arithmetic_calculator_with_compose.composable_uis
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.gauravtak.arithmetic_calculator_with_compose.CalculatorViewModel
 
 @Composable
@@ -16,7 +16,7 @@ fun CalculatorUiScreen(viewModel: CalculatorViewModel? = null) {
     val currentExpression = viewModel?.getCurrentExpression()?.observeAsState()?.value
     val result = viewModel?.getResult()?.observeAsState()?.value
 
-    Column(modifier = Modifier.fillMaxHeight()) {
+    Column(modifier = Modifier.fillMaxHeight().fillMaxWidth().padding(horizontal = 10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
         Box(modifier = Modifier.weight(0.33f)) {
             InputAreaUI(currentExpression, result)
         }
